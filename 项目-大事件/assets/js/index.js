4cc5d536f460d237c0633a11b3acc929d3bd7f86
dupanfeng <17339762006.com>
2021-06-15 19:08:44 +0800

    baseapi

--- 项目-大事件/assets/js/index.js
+++ 项目-大事件/assets/js/index.js
@@ -2,6 +2,18 @@ $(function() {
         //获取用户的基本信息   
         getuserInfo()
 
+        var layer = layui.layer
+        $('#btnLogout').on('click', function() {
+            //提示用户是否退出
+            layer.confirm('确定退出？', { icon: 3, title: '提示' },
+                function(index) {
+                    //1.清空本地存储中的token
+                    localStorage.removeItem('token')
+                        // 2.重新跳转到登录页
+                    location.href = 'login.html'
+                    layer.close(index);
+                });
+        })
 
 
 
@@ -25,7 +37,19 @@ function getuserInfo() {
             // 调用 renderAvatar 渲染用户的头像
             renderAvatar(res.data)
 
-        }
+        },
+        //不论成功还是失败，最终都会调用complete回调函数
+        // complete: function(res) {
+        //     //    console.log('执行了 complete 回调：')
+        //     //   console.log(res)
+        //     //   在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //             // 1. 强制清空 token
+        //         localStorage.removeItem('token')
+        //                 // 2. 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
 
     })
 }
